2f4ff1b751dd34edffeefd5d48034bfefcfe3cfd
AgustianAr <112686936+AgustianAr@users.noreply.github.com>
2024-03-21 22:38:14 +0800

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>navbar</title>
+</head>
+<body>
+    <nav>
+            <div class="menu">
+                <ul>
+                    <li><a href="#profile">About Me</a></li>
+                    <li><a href="#exp">Certificates</a></li>
+                </ul>
+            </div>
+    </nav>
+</body>
+</html>
